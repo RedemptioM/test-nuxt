dc20341d0670351d63f838ae38396cf87bcdff4b
Damon <ccmxc1991@gmail.com>
2019-03-22 02:59:59 +1100

    all done

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
       methods and pages due to limited time as currently I'm also in a
       programming competition on top of working. There is some auto-generate
       code which has no effect to the result, so I just leave it there. Sorry
-      about that.
+      about that. Nuxt is good, I learned a lot from this task, thank you guys.
     </h1>
   </div>
 </template>
