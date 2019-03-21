8c3645f9d458144267d96ee3c749dcd525ff2f10
Damon <ccmxc1991@gmail.com>
2019-03-22 02:38:58 +1100

    Finished the requirements

---
+++ pages/user/_username.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1 .display-3>{{ userName }}'s page</h1>
+    <h1 .display-2>User id: {{ userNum }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  asyncData({ params }) {
+    return {
+      userName: params.username
+        .split('-')
+        .slice(0, 2)
+        .map(item => item.charAt(0).toUpperCase() + item.slice(1))
+        .join(' '),
+      userNum: parseInt(params.username.match(/[0-9]/g))
+    }
+  },
+  head() {
+    return {
+      title: `${this.userName}'s page`,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: `This is ${this.userName}'s page, the number is ${
+            this.userNum
+          }`
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped></style>
